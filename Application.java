e32428813d11836d92c866e4187258847d273af7
aezBurkhal <44988851+aezBurkhal@users.noreply.github.com>
2018-12-12 00:15:32 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,26 @@
+
+public class Application {// indent to organize
+	public static void main(String[] args) {
+	
+		int myInt = 7;
+		
+		String text = "Hello"; 
+		
+		String blank = " ";
+		
+		String name = "Bob";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		//some ways to output text and work variables
+		System.out.println("Hello" + " " + "Bob");
+		double myDouble = 7.8;
+		System.out.println("My number is: " + myDouble + ".");
+		System.out.println("My integer is:" + myInt);
+}
+	
+		
+	}
+
+
